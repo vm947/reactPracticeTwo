2beb17eeb8c4774d5bc60bc692a2d34c84aed371
vm947 <vm947@nyu.edu>
2020-04-12 19:49:04 +0300

    changes

--- src/components/common/tableHeader.jsx
+++ src/components/common/tableHeader.jsx
@@ -11,17 +11,25 @@ class TableHeader extends Component {
     }
     this.props.onSort(sortColumn);
   };
+
+  renderSortIcon = (column) => {
+    const { sortColumn } = this.props;
+    if (column.path !== sortColumn.path) return null;
+    if (sortColumn.order === "asc") return <i className="fa fa-sort-asc"></i>;
+    return <i className="fa fa-sort-descending"></i>;
+  };
+
   render() {
     return (
       <thead>
         <tr>
-          {this.props.columns.map(column => (
+          {this.props.columns.map((column) => (
             <th
-                key = {column.path || column.key}
+            className ="clickable"
+              key={column.path || column.key}
               onClick={() => this.raiseSort(column.path)}
-              style={{ cursor: "pointer" }}
             >
-              {column.label}
+              {column.label} {this.renderSortIcon(column)}
             </th>
           ))}
         </tr>
