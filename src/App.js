3c55a5f3dea42ebb9d84a9f110b507a3e889ebbf
vm947 <vm947@nyu.edu>
2020-04-15 19:57:44 +0300

    branch changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
-import React from 'react';
-import './App.css';
-import Movies from './components/movies'
+import React from "react";
+import { Route, Switch, Redirect } from "react-router-dom";
+import Movies from "./components/movies";
+import MovieForm from "./components/common/movieForm";
+import NavBar from "./components/navBar";
+import Customers from "./components/common/customers";
+import Rentals from "./components/common/rentals";
+import NotFound from "./components/common/notFound";
+import "./App.css";
 
 function App() {
   return (
-    <main className = "container">
-      <Movies/>
-    </main>
+    <div className="mt-0">
+      <NavBar />
+      <main className="container">
+        <Switch>
+          <Route path="/movies/:id" component={MovieForm} />
+          <Route path="/movies" component={Movies} />
+          <Route path="/customers" component={Customers} />
+          <Route path="/rentals" component={Rentals} />
+          <Route path="/not-found" component={NotFound} />
+          <Redirect from="/" exact to="/movies" />
+          <Redirect to="/not-found" />
+        </Switch>
+      </main>
+    </div>
   );
 }
 
---
+++ src/components/navBar.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { NavLink, Link } from "react-router-dom";
+
+const NavBar = (props) => {
+  return (
+    <nav className="nav nav-dark bg-dark mb-4">
+      <Link
+        className="nav-link"
+        style={{ fontWeight: "bold", color: "white", fontSize: "1.5rem" }}
+        to="/movies"
+      >
+        Vidly
+      </Link>
+      <NavLink className="nav-link" to="/movies">
+        Movies
+      </NavLink>
+      <NavLink className="nav-link" to="/customers">
+        Customers
+      </NavLink>
+      <NavLink className="nav-link" to="/rentals" tabindex="-1" aria-disabled="true">
+        Rentals
+      </NavLink>
+    </nav>
+  );
+};
+
+export default NavBar;
