e65f27d5679a6c28d3ba8a8094e8748694e1bbf9
vm947 <vm947@nyu.edu>
2020-04-16 16:05:50 +0300

    changes

---
+++ src/components/loginForm.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class LoginForm extends Component {
+    
+    render() { 
+        return ( <h1>Login</h1>  );
+    }
+}
+ 
+export default LoginForm ;
