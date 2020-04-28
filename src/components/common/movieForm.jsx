5292a4d0102f02caa5c40fb58ea72c12b70c7987
vm947 <vm947@nyu.edu>
2020-04-28 08:22:04 +0300

    changes

--- src/components/common/movieForm.jsx
+++ src/components/common/movieForm.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import Joi from "joi-browser";
 import Form from "./form";
-import { getMovie, saveMovie } from "../../services/fakeMovieService";
-import { getGenres } from "../../services/fakeGenreService";
+import { getMovie, saveMovie } from "../../services/movieService";
+import { getGenres } from "../../services/genreService";
 
 class MovieForm extends Form {
   state = {
@@ -32,8 +32,8 @@ class MovieForm extends Form {
       .label("Daily Rental Rate"),
   };
 
-  componentDidMount() {
-    const genres = getGenres();
+  async componentDidMount() {
+    const { data: genres } = await getGenres();
     this.setState({ genres });
     //component loads and genres is populated into the state
 
@@ -42,14 +42,17 @@ class MovieForm extends Form {
     //the params are checked. the /:id of the route. If route is new--empty page--then below is not executed
     //if params is /:id then below is executed
 
-    const movie = getMovie(movieId);
-    if (!movie) return this.props.history.replace("/not-found");
-    //the moviedId (with params) is grabbed from the bullshit fakeMovie service js doc with the bullshit function.
-    //it retrieves the object about the movie with that specific id, based on the params
-
-    this.setState({ data: this.mapToViewModel(movie) });
-    //the grabbed object is then converted into an object with mapToViewModel THEN STORED IN THE STATE
-    //the updated state is used by the METHODS IN INPUT.JSX
+    try {
+      const { data: movie } = await getMovie(movieId);
+      this.setState({ data: this.mapToViewModel(movie) });
+      //the grabbed object is then converted into an object with mapToViewModel THEN STORED IN THE STATE
+      //the updated state is used by the METHODS IN INPUT.JSX
+    } catch (error) {
+      if (error /*error.response && error.response.status === 404*/)
+        return this.props.history.replace("/not-found");
+      //the moviedId (with params) is grabbed from the bullshit fakeMovie service js doc with the bullshit function.
+      //it retrieves the object about the movie with that specific id, based on the params
+    }
   }
 
   mapToViewModel(movie) {
@@ -69,19 +72,19 @@ class MovieForm extends Form {
   };
 
   render() {
-    //each render method below has its own state(seen in input.jsx--initially empty), 
+    //each render method below has its own state(seen in input.jsx--initially empty),
     //When it is called it uses the state of movieForm--which is generated from componentDidMount()
     return (
       <div>
         <h1>Movie Form</h1>
         <form onSubmit={this.handleSubmit}>
-          {this.renderInput("title", "Title")} 
+          {this.renderInput("title", "Title")}
           {this.renderSelect("genreId", "Genre", this.state.genres)}
           {this.renderInput("numberInStock", "Number in Stock", "number")}
           {this.renderInput("dailyRentalRate", "Rate")}
           {this.renderButton("Save")}
         </form>
-      </div> 
+      </div>
     );
   }
 }
